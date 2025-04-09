2b77d28841708a982ba156ab3979af23deb9625e
Colin Maudry <colin@maudry.com>
2025-04-09 19:12:55 +0200

    Basic config

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,3 +3,23 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
+myynh_setup_python_venv() {
+    # Always recreate everything fresh with current python version
+    ynh_secure_remove "$data_dir/venv"
+
+    # Skip pip because of: https://github.com/YunoHost/issues/issues/1960
+    python3 -m venv --without-pip "$data_dir/venv"
+
+    chown -c -R "$app:" "$data_dir"
+
+    # run source in a 'sub shell'
+    (
+        set +o nounset
+        source "$data_dir/venv/bin/activate"
+        set -o nounset
+        set -x
+        ynh_exec_as $app $data_dir/venv/bin/python3 -m ensurepip
+        ynh_exec_as $app $data_dir/venv/bin/pip3 install --upgrade wheel pip setuptools
+        ynh_exec_as $app $data_dir/venv/bin/pip3 install -r "$data_dir/requirements.txt"
+    )
+}
